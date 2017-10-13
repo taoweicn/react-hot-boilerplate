09a4130b3587cc7d88a04b14142296be471e3e68
TaoWei <twchn@live.com>
2017-10-13 21:57:54 +0800

    Update webpack config

--- app/app.jsx
+++ app/app.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function Hello() {
-    return (
-        <div>
-            Hello World!
-        </div>
-    )
+  return (
+    <div>
+      Hello World!
+    </div>
+  );
 }
 
 const app = document.createElement('div');
 document.body.appendChild(app);
-ReactDOM.render(<Hello/>, app);
+ReactDOM.render(<Hello />, app);
--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,47 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: {
-        app: './app/app.jsx'
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist',
-        hot: true
-   },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: 'css-loader'
-            },
-            {
-                test: /\.jsx$/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['latest', 'react']
-                    }
-                }
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'My webpack demo'
-        }),
-        new webpack.HotModuleReplacementPlugin()
+  entry: {
+    app: './app/app.jsx'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devtool: 'inline-source-map',
+  //开启webpack dev server
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: 'css-loader'
+      },
+      {
+        test: /\.jsx$/,
+        enforce: "pre",
+        use: 'eslint-loader'
+      },
+      {
+        test: /\.jsx$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['latest', 'react']
+          }
+        }
+      }
     ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'My webpack demo'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
 }

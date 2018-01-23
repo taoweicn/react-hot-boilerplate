57625ffde3a39727805e656648f26a27510ff04d
taoweicn <twchn@live.com>
2018-01-23 21:38:53 +0800

    Move webpack config files to directory

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -6,6 +6,11 @@ const common = require('./webpack.common');
 module.exports = merge(common, {
   devtool: 'source-map',
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
     new UglifyJsPlugin({
       sourceMap: true
     }),
